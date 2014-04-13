62f5e54e8114566ddd84556cbcde2e5158963bb1
rCori <rcori@ucsc.edu>
2014-04-12 18:53:39 -0700

    Work continues on the tile manager. I have every tile I need represented for PacMan I think. Each tile renders in some hacky way with drect drawing on the canvas. I don't have anything in the way of a sprite manager so it will do for now. I can write that part later.

--- pacman.js
+++ pacman.js
@@ -6,8 +6,8 @@
 
 //Much of the code here inspirded from a tutorial by Daniel X. Moore
 //http://www.html5rocks.com/en/tutorials/canvas/notearsgame/
-var CANVAS_WIDTH = 400;
-var CANVAS_HEIGHT = 600;
+var CANVAS_WIDTH = 448;
+var CANVAS_HEIGHT = 576;
 
 //This code puts the canvas we want in the browser
 var canvasElement = jQuery("<canvas width='" + CANVAS_WIDTH +
@@ -17,6 +17,18 @@ var myCanv = canvasElement.get(0);
 canvasElement.appendTo('body');
 
 //Lets call this a debug map
-myMapData = [['o','o','o'],['M','=','3'],['2','3','6']];
-var myTileset = tileset(myMapData);
+/*Original pacman has a tileset of 28x36 so thats what we
+ *will be going for here. Of course we will need to draw over
+ *some of this as well.
+ *If the game is 448x576 each tile is (448/28)x(576x36) or 16x16.
+ * Eventually each tile will be a 16x16 pixel image
+ */
+//myMapData = [['=','I','I'],['=','o','='],['o','O','o'],['I','6','I']];
+/* Now we are going to try a real Pacman map tile set
+ * minus icons and ghost pen door
+ * this is according to the pacman dossier
+ */
+var myMapData = [['M','M','M','3','4','4','4','4','4','4','4','4','9','M','M','M','8','e','2','M','M','M','7','4','4','4','9','3','4','4','4','4','9','M','M'],
+				 ['M','M','M','8','o','o','O','o','o','o','o','o','3','M','M','M','8','e','2','M','M','M','8','o','o','O','=','=','o','o','o','o','3','M','M']]
+var myTileset = tileset(myMapData,CANVAS_WIDTH, CANVAS_HEIGHT);
 myTileset.renderMap(canvas);
--- tiles.js
+++ tiles.js
@@ -3,9 +3,30 @@
  * Here goes nothing.
  */
 
+//Make sure your data is a uniform matrix
+function checkMatrix(matrixHopeful){
+	//A return value of 0 means your matrix has uniform lengths
+	//A return value of 1 means it is not a matrix, not uniform row lengths
+	//A return value of 2 means this isn't even a 2D array we could process
+	var returnCode = 0;
+	try{
+		var size = matrixHopeful[0].length;
+		for(var k = 0; k<matrixHopeful.length; k++){
+			if(matrixHopeful[k].length != size){
+				returnCode = 1;
+				break;
+			}
+		}
+		return returnCode;
+	}
+	catch(err){
+		returnCode = 2;
+		return returnCode;
+	}
+}
 
 //Tileset constructor
-function tileset(tileData){
+function tileset(tileData, width, height){
 	var I = {};
 
 	//Map needs to be a 2D array of data
@@ -28,66 +49,128 @@ function tileset(tileData){
 	 * 'e' is a space that pacman can move but has no dot
 	 */
 
+	if(checkMatrix(tileData) != 0){
+		I.valid = false;
+		return I;
+	}
+	else{
+		I.valid = true;
+	}
+
+	I.tileWidth = width/I.map.length;
+	I.tileHeight = height/I.map[0].length;
+
 	I.renderMap = function(canvas){
 	 	for(var j = 0; j<I.map.length; j++){
 	 		for(var k = 0; k<I.map[j].length; k++){
 	 			//Now draw the right piece for I.map[j][k]
 	 			switch(I.map[j][k]){
 	 			case 'o':
+	 				//draw pellet tile
+	 				canvas.fillStyle = "#000000";
+	 				canvas.fillRect(I.tileWidth * j, I.tileHeight * k, I.tileWidth, I.tileHeight);
 	 				canvas.fillStyle = "#FFFF00";
-	 				canvas.fillRect((400/I.map.length) * j, (600/I.map[j].length) * k, 10, 10);
+	 				canvas.fillRect((I.tileWidth * j)+(I.tileWidth/3), (I.tileHeight * k)+(I.tileHeight/3), I.tileWidth/3, I.tileHeight/3);
 	 				console.log('Dot tile at ' + j + ', ' + k);
 	 				break;
 	 			case 'O':
 	 				//draw energizer tile
+	 				canvas.fillStyle = "#000000";
+	 				canvas.fillRect(I.tileWidth * j, I.tileHeight * k, I.tileWidth, I.tileHeight);
+	 				canvas.fillStyle = "#FFFFFF";
+	 				canvas.fillRect((I.tileWidth * j)+(I.tileWidth/4), (I.tileHeight * k)+(I.tileHeight/4), I.tileWidth/2, I.tileHeight/2);
 	 				console.log('Energizer tile at ' + j + ', ' + k);
 	 				break;
 	 			case '=':
 	 				//draw straight horizontal piece tile tile
+	 				canvas.fillStyle = "#000000";
+	 				canvas.fillRect(I.tileWidth * j, I.tileHeight * k, I.tileWidth, I.tileHeight);
+	 				canvas.fillStyle = "#0000FF";
+	 				canvas.fillRect(I.tileWidth * j, (I.tileHeight * k)+(I.tileHeight/3), I.tileWidth, I.tileHeight/3);
 	 				console.log('straight horiz piece tile at ' + j + ', ' + k);
 	 				break;
 	 			case 'I':
 	 				//draw straight vertical piece tile
+	 				canvas.fillStyle = "#000000";
+	 				canvas.fillRect(I.tileWidth * j, I.tileHeight * k, I.tileWidth, I.tileHeight);
+	 				canvas.fillStyle = "#0000FF";
+	 				canvas.fillRect((I.tileWidth * j)+(I.tileWidth/3), I.tileHeight * k, I.tileWidth/3, I.tileHeight);
 	 				console.log('straight vert piece tile at ' + j + ', ' + k);
 	 				break;
 	 			case '6':
 	 				//draw right end piece tile
+	 				canvas.fillStyle = "#000000";
+	 				canvas.fillRect(I.tileWidth * j, I.tileHeight * k, I.tileWidth, I.tileHeight);
+	 				canvas.fillStyle = "#0000FF";
+	 				canvas.fillRect(I.tileWidth * j, I.tileHeight * k, 2*(I.tileWidth/3), I.tileHeight);
 	 				console.log('Right end piece tile at' + j + ', ' + k);
 	 				break;
 	 			case '4':
 	 				//draw left end piece  tile
+	 				canvas.fillStyle = "#000000";
+	 				canvas.fillRect(I.tileWidth * j, I.tileHeight * k, I.tileWidth, I.tileHeight);
+	 				canvas.fillStyle = "#0000FF";
+	 				canvas.fillRect((I.tileWidth * j) + (I.tileWdith/3), I.tileHeight * k, 2*(I.tileWidth/3), I.tileHeight);
 	 				console.log('Left end piece tile at ' + j + ', ' + k);
 	 				break;
 	 			case '8':
 	 				//draw downward end piece tile
+	 				canvas.fillStyle = "#000000";
+	 				canvas.fillRect(I.tileWidth * j, I.tileHeight * k, I.tileWidth, I.tileHeight);
+	 				canvas.fillStyle = "#0000FF";
+	 				canvas.fillRect((I.tileWidth * j)+(I.tileWidth/3), I.tileHeight * k, I.tileWidth/3, I.tileHeight-(I.tileHeight/3));
 	 				console.log('Downward end piece tile at ' + j + ', ' + k);
 	 				break;
 				case '2':
 	 				//draw upward end piece tile
+	 				canvas.fillStyle = "#000000";
+	 				canvas.fillRect(I.tileWidth * j, I.tileHeight * k, I.tileWidth, I.tileHeight);
+	 				canvas.fillStyle = "#0000FF";
+	 				canvas.fillRect((I.tileWidth * j)+(I.tileWidth/3), (I.tileHeight * k)+(I.tileHeght/3), I.tileWidth/3, 2*(I.tileHeight/3));
 	 				console.log('Upward end piece tile at ' + j + ', ' + k);
 	 				break;
 	 			case '3':
 	 				//draw downright corner piece tile
+	 				canvas.fillStyle = "#000000";
+	 				canvas.fillRect(I.tileWidth * j, I.tileHeight * k, I.tileWidth, I.tileHeight);
+	 				canvas.fillStyle = "#0000FF";
+	 				canvas.fillRect((I.tileWidth * j)+(2*(I.tileWidth/3)), (I.tileHeight * k)+(2*(I.tileHeight/3)), I.tileWidth/3, I.tileHeight/3);
 	 				console.log('Downright corner piece tile at ' + j + ', ' + k);
 	 				break;
 	 			case '1':
 	 				//draw downleft corner piece tile
+	 				canvas.fillStyle = "#000000";
+	 				canvas.fillRect(I.tileWidth * j, I.tileHeight * k, I.tileWidth, I.tileHeight);
+	 				canvas.fillStyle = "#0000FF";
+	 				canvas.fillRect(I.tileWidth * j, (I.tileHeight * k)+(2*(I.tileHeight/3)), I.tileWidth/3, I.tileHeight/3);
 	 				console.log('Downleft corner piece tile at ' + j + ', ' + k);
 	 				break;
 	 			case '7':
 	 				//draw upleft corner piece tile
+	 				canvas.fillStyle = "#000000";
+	 				canvas.fillRect(I.tileWidth * j, I.tileHeight * k, I.tileWidth, I.tileHeight);
+	 				canvas.fillStyle = "#0000FF";
+	 				canvas.fillRect(I.tileWidth * j, I.tileHeight * k, I.tileWidth/3, I.tileHeight/3);
 	 				console.log('upleft corner piece tile at ' + j + ', ' + k);
 	 				break;
 	 			case '9':
-	 				//draw upright corener piece tile
+	 				//draw upright corner piece tile
+	 				canvas.fillStyle = "#000000";
+	 				canvas.fillRect(I.tileWidth * j, I.tileHeight * k, I.tileWidth, I.tileHeight);
+	 				canvas.fillStyle = "#0000FF";
+	 				canvas.fillRect((I.tileWidth * j)+(2*(I.tileWidth/3)), I.tileHeight * k, I.tileWidth/3, I.tileHeight/3);
 	 				console.log('upright corner piece tile at ' + j + ', ' + k);
 	 				break;
 	 			case 'M':
 	 				//draw a piece inside the barrier(probably a solid color)
+	 				canvas.fillStyle = "#000000";
+	 				canvas.fillRect(I.tileWidth * j, I.tileHeight * k, I.tileWidth, I.tileHeight);
 	 				console.log('Inner barrier tile at ' + j + ', ' + k);
 	 				break;
 	 			case 'e':
 	 				//draw a blank movable space
+	 				canvas.fillStyle = "#000000";
+	 				canvas.fillRect(I.tileWidth * j, I.tileHeight * k, I.tileWidth, I.tileHeight);
 	 				console.log('Blank movable space tile at ' + j + ', ' + k);
 	 				break;
 	 			}
